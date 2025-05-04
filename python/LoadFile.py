7edae18973ee148294235f0aae33a877d4596df9
yoshi-jun <jun1.yossy@gmail.com>
2025-05-04 11:51:30 +0900

    adjust

---
+++ python/LoadFile.py
@@ -0,0 +1,29 @@
+
+import time 
+LoadedData=[[]for _ in range(200)]
+
+#------------------------------------------------------------------------------
+def LoadFile(n):
+    f_name = "input/"+str(n)+".txt"
+    with open(f_name, 'r', encoding='utf-8') as f:
+        content = f.read()
+    
+    content = content.split("\n")
+
+    LoadedData[n-1].append(content[0])
+    LoadedData[n-1].append(content[1])
+
+#==============================================================================
+def main():
+
+    for n in range(1,200):
+        LoadFile(n)
+
+if __name__=="__main__":
+
+    start = time.time()
+    main()
+    print(LoadedData)
+    end   = time.time()
+
+    print(end-start)
